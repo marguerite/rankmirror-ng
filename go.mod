03ebbbb7ea2cc8a60e0b7ae60e9396bb59f85f5d
marguerite <i@marguerite.su>
2019-12-04 12:41:11 +0800

    [fix]net.LookupHost no such host error. golang's native dns lookup only supports *url.URL's Host (no path, no http(s):// prefix)

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/marguerite/rankmirror-ng
+
+require (
+	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
+	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
+	golang.org/x/net v0.0.0-20191204025024-5ee1b9f4859a // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20191204011308-9611592c72f6 // indirect
+	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
+)
--- rankmirror-ng.go
+++ rankmirror-ng.go
@@ -1,18 +1,39 @@
 package main
 
 import (
-	"github.com/tatsushid/go-fastping"
-	"log"
-	//"flag"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
+	"net/url"
+	"regexp"
 	"time"
+
+	"github.com/tatsushid/go-fastping"
 )
 
-func pingMirror(mirror string) time.Duration {
+type MirrorList []Mirror
+
+type Mirror struct {
+	Name        string
+	Raw         string
+	URI         *url.URL
+	Distro      string
+	Version     string
+	GeoLocation string
+	IPv6        bool
+}
+
+func (m Mirror) Ping() time.Duration {
 	var t time.Duration
+	protocol := "ip4:icmp"
 	pinger := fastping.NewPinger()
-	ra, err := net.ResolveIPAddr("ip4:icmp", mirror)
+	if m.IPv6 {
+		protocol = "ip6:icmp"
+	}
+
+	ra, err := net.ResolveIPAddr(protocol, m.URI.Host)
 	if err != nil {
 		log.Println(err)
 		return t
@@ -29,19 +50,73 @@ func pingMirror(mirror string) time.Duration {
 	return t
 }
 
-func averageDuration(mirror string, times int) float64 {
-	var durations []time.Duration
-	var sum float64
-	for i := 0; i < times; i++ {
-		durations = append(durations, pingMirror(mirror))
+func contains(a []string, x string) bool {
+	for _, v := range a {
+		if x == v {
+			return true
+		}
 	}
-	for _, j := range durations {
-		sum += float64(j / time.Millisecond)
+	return false
+}
+
+func osInfo() (id, version string) {
+	f, e := ioutil.ReadFile("os-release")
+	if e != nil {
+		log.Println("can't open /etc/os-release")
+		log.Fatal(e)
 	}
-	return sum / float64(times)
+
+	SUPPORTED_OS := []string{"tumbleweed", "leap", "fedora", "arch"}
+
+	id_r := regexp.MustCompile(`(?m)^ID=("opensuse-)?([^"]+)(")?$`)
+	ver_r := regexp.MustCompile(`(?m)^VERSION_ID=(")?([^"]+)(")?$`)
+
+	id = id_r.FindStringSubmatch(string(f))[2]
+
+	if !contains(SUPPORTED_OS, id) {
+		log.Fatal("Unsupported OS: " + id)
+	}
+
+	version = "0.0"
+	if id == "leap" || id == "fedora" {
+		version = ver_r.FindStringSubmatch(string(f))[2]
+	}
+
+	return id, version
+}
+
+func (m Mirror) FullURL() string {
+	ma := make(map[string]string)
+	ma["leap"] = "/distribution/leap/" // 15.0/repo/oss
+	ma["tumbleweed"] = "/tumbleweed/repo/oss/"
+	ma["fedora"] = "/releases/" // 28/Everything/x86_64/os/
+	ma["arch"] = "/core/os/x86_64/"
+
+	url := m.Raw + ma[m.Distro]
+
+	if m.Distro == "leap" {
+		url += m.Version + "/repo/oss/"
+	}
+
+	if m.Distro == "fedora" {
+		url += m.Version + "/Everything/x86_64/os/"
+	}
+
+	return url
 }
 
 func main() {
-	addr := "f1234klll.com"
-	fmt.Println(pingMirror(addr))
+	var geo string
+	var ipv6 bool
+
+	flag.StringVar(&geo, "geo", "china", "geo location of the mirror.")
+	flag.BoolVar(&ipv6, "ipv6", false, "check ipv6 only.")
+
+	flag.Parse()
+
+	distro, version := osInfo()
+	raw := "https://mirrors.aliyun.com/opensuse"
+	uri, _ := url.Parse(raw)
+	m := Mirror{"阿里云", raw, uri, distro, version, geo, ipv6}
+	fmt.Println(m.Ping())
 }
