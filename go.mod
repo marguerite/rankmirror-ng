dccc1ba77353e0186820a1e4b415e78fbedbf21b
marguerite <i@marguerite.su>
2019-12-05 14:51:13 +0800

    add geolocation feature

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/marguerite/rankmirror-ng
 require (
 	github.com/alexkohler/nakedret v1.0.0 // indirect
 	github.com/opennota/check v0.0.0-20180911053232-0c771f5545ff // indirect
+	github.com/oschwald/geoip2-golang v1.3.0
+	github.com/oschwald/maxminddb-golang v1.5.0 // indirect
 	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
 	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
 	golang.org/x/net v0.0.0-20191204025024-5ee1b9f4859a // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	golang.org/x/sys v0.0.0-20191204072324-ce4227a45e2e // indirect
 	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20191204011308-9611592c72f6 // indirect
-	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
+	golang.org/x/tools v0.0.0-20191205060818-73c7173a9f7d // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.7
 )
--- rankmirror-ng.go
+++ rankmirror-ng.go
@@ -6,27 +6,154 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 	"net/url"
+	"path"
 	"regexp"
+	"strings"
 	"time"
 
+	geoip2 "github.com/oschwald/geoip2-golang"
 	"github.com/tatsushid/go-fastping"
+	yaml "gopkg.in/yaml.v2"
 )
+
+func readMirrorList(file string) (MirrorList, error) {
+	mirrorlist := MirrorList{}
+
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return mirrorlist, err
+	}
+
+	err = yaml.Unmarshal(b, &mirrorlist)
+	if err != nil {
+		return mirrorlist, err
+	}
+
+	return mirrorlist, nil
+}
 
 type MirrorList []Mirror
 
+func (mirrorlist *MirrorList) preload(force bool) {
+	for i := range *mirrorlist {
+		err := (*mirrorlist)[i].preload(force)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
+}
+
 type Mirror struct {
-	Name             string `yaml:"name"`
-	Raw              string `yaml:"raw"`
-	URI              *url.URL
+	Name             string        `yaml:"name"`
+	Raw              string        `yaml:"raw"`
 	Distro           string        `yaml: "distro"`
 	Version          []string      `yaml: "version"`
-	GeoLocation      string        `yaml: "geo"`
-	PhysicalDistance string        `yaml: "physical-distance"`
-	RouteLength      string        `yaml: "route-length"`
-	PingSpeed        time.Duration `yaml: "ping-speed"`
-	DownloadSpeed    string        `yaml: "download-speed"`
-	IPv6             bool          `yaml: ipv6`
+	Country          string        `yaml: "country"`
+	Latitude         float64       `yaml: "latitude"`
+	Longitude        float64       `yaml: "longitude"`
+	PhysicalDistance string        `yaml: "distance"`
+	RouteLength      string        `yaml: "route"`
+	PingSpeed        time.Duration `yaml: "ping"`
+	DownloadSpeed    string        `yaml: "download"`
+	IPv6             bool          `yaml: "ipv6"`
+}
+
+func (m *Mirror) preload(force bool) error {
+	// Raw is a must field
+	if len(m.Raw) == 0 {
+		return fmt.Errorf("raw field is required: %v", *m)
+	}
+
+	if len(m.Name) == 0 {
+		re := regexp.MustCompile(`([\.\/])?(\w+)\.\w+($|\/)`)
+		m.Name = strings.Title(re.FindStringSubmatch(m.Raw)[2])
+	}
+
+	if len(m.Distro) == 0 {
+		re := regexp.MustCompile(`\/(\w+)$`)
+		if re.MatchString(m.Raw) {
+			m.Distro = re.FindStringSubmatch(m.Raw)[1]
+		} else {
+			return fmt.Errorf("distro field is empty and can not combine one from raw field: %v", *m)
+		}
+	}
+
+	if len(m.Version) == 0 {
+		m.Version = probeDistroVersions(m.Distro, m.Raw)
+	}
+
+	if len(m.Country) == 0 {
+		var err error
+		m.Country, m.Latitude, m.Longitude, err = probeGeoLocation(m.Raw)
+		if err != nil {
+			return fmt.Errorf("probeGeoLocation failed: %v, %v", err, *m)
+		}
+	}
+
+	if m.Latitude == 0 || m.Longitude == 0 {
+		var err error
+		_, m.Latitude, m.Longitude, err = probeGeoLocation(m.Raw)
+		if err != nil {
+			return fmt.Errorf("probeGeoLocation failed: %v, %v", err, *m)
+		}
+	}
+
+	return nil
+}
+
+func probeDistroVersions(distro, raw string) []string {
+	versions := []string{}
+	paths := []string{}
+	suffix := ""
+
+	switch distro {
+	case "opensuse":
+		paths = []string{"distribution/leap/15.1", "distribution/leap/15.2", "distribution/leap/15.3", "tumbleweed"}
+		suffix = "repo/oss"
+	}
+
+	timeout := time.Duration(3 * time.Second)
+
+	for _, p := range paths {
+		// goroutine?
+		uri, _ := url.Parse(raw)
+		uri.Path = path.Join(uri.Path, p, suffix)
+		c := &http.Client{Timeout: timeout}
+		// redirect means it's there for Aliyun
+		if strings.Contains(raw, "aliyun.com") {
+			c.CheckRedirect = func(req *http.Request, via []*http.Request) error { return http.ErrUseLastResponse }
+		}
+		resp, err := c.Get(uri.String())
+		if err != nil || resp.StatusCode != 404 {
+			versions = append(versions, p)
+		}
+	}
+
+	return versions
+}
+
+func probeGeoLocation(raw string) (string, float64, float64, error) {
+	uri, _ := url.Parse(raw)
+	ra, err := net.ResolveIPAddr("ip4", uri.Host)
+	if err != nil {
+		return "", 0, 0, err
+	}
+	ip := net.ParseIP(ra.String())
+
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		return "", 0, 0, err
+	}
+	defer db.Close()
+
+	record, err := db.City(ip)
+	if err != nil {
+		return "", 0, 0, err
+	}
+	return record.Country.Names["en"], record.Location.Latitude, record.Location.Longitude, nil
 }
 
 func (m Mirror) Ping() time.Duration {
@@ -37,7 +164,9 @@ func (m Mirror) Ping() time.Duration {
 		protocol = "ip6:icmp"
 	}
 
-	ra, err := net.ResolveIPAddr(protocol, m.URI.Host)
+	uri, _ := url.Parse(m.Raw)
+
+	ra, err := net.ResolveIPAddr(protocol, uri.Host)
 	if err != nil {
 		log.Println(err)
 		return t
@@ -99,11 +228,11 @@ func (m Mirror) FullURL() string {
 	url := m.Raw + ma[m.Distro]
 
 	if m.Distro == "leap" {
-		url += m.Version + "/repo/oss/"
+		url += m.Version[0] + "/repo/oss/"
 	}
 
 	if m.Distro == "fedora" {
-		url += m.Version + "/Everything/x86_64/os/"
+		url += m.Version[0] + "/Everything/x86_64/os/"
 	}
 
 	return url
@@ -118,9 +247,14 @@ func main() {
 
 	flag.Parse()
 
-	distro, version := osInfo()
-	raw := "https://mirrors.aliyun.com/opensuse"
-	uri, _ := url.Parse(raw)
-	m := Mirror{"阿里云", raw, uri, distro, version, geo, ipv6}
-	fmt.Println(m.Ping())
+	//distro, version := osInfo()
+	//raw := "https://mirrors.aliyun.com/opensuse"
+	//uri, _ := url.Parse(raw)
+	//m := Mirror{"阿里云", raw, uri, distro, []string{version}, geo, ipv6}
+	//fmt.Println(m.Ping())
+
+	mirrorlist, _ := readMirrorList("mirrorlist.yaml")
+	fmt.Println(mirrorlist)
+	mirrorlist.preload(false)
+	fmt.Println(mirrorlist)
 }
