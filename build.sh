0143c7bb3c03d4bdfeee52ca68689b8fd4076b42
marguerite <i@marguerite.su>
2021-07-04 11:51:40 +0800

    add Build CI

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-go mod download
-go mod vendor
+mkdir -p rankmirror-ng.AppDir/usr/bin
 go build -o rankmirror-ng.AppDir/usr/bin/rankmirror-ng
 cp mirrors.yaml rankmirror-ng.AppDir/usr
 wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
 wget https://github.com/AppImage/AppImageKit/releases/download/12/AppRun-x86_64
-mv AppRun-x86_64 rankmirror-ng.AppDir/AppRun
+cp -r AppRun-x86_64 rankmirror-ng.AppDir/AppRun
+chmod +x rankmirror-ng.AppDir/AppRun
+cp -r rankmirror-ng.desktop rankmirror-ng.AppDir
+cp -r yast-services-manager.svg rankmirror-ng.AppDir
 ./appimagetool-x86_64.AppImage rankmirror-ng.AppDir
-mv rankmirror-ng-x86_64.AppImage rankmirror-ng-v2-1.0.0.x86_64.AppImage
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/marguerite/rankmirror-ng
 go 1.15
 
 require (
-	github.com/fatih/color v1.10.0
-	github.com/marguerite/diagnose v0.0.0-20201108054916-cac4196aae2d
-	github.com/marguerite/go-stdlib v0.0.0-20210319061925-9cba1a57e232
+	github.com/fatih/color v1.12.0
+	github.com/marguerite/diagnose v0.0.0-20210608143816-5198a120fc78
+	github.com/marguerite/go-stdlib v0.0.0-20210704033045-fb80b42257fa
 	gopkg.in/yaml.v2 v2.4.0
 )
